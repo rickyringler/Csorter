2fd07def1db19be1a101b959d767264d91d93760
Ricky Ringler <richard.rringler@gmail.com>
2023-12-01 18:33:08 -0500

    Add project files.

---
+++ Csorter/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+std::string sys_download_file_path = "C:/Users/User/Downloads/test";
+std::string* ptr_sys = &sys_download_file_path;
+
+int main(int argc, char *argv[])
+{
+	
+
+	return 0;
+}
